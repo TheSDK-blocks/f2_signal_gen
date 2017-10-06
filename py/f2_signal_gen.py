41bae95963fb54853a883e872f0d340f3be17951
Marko Kosunen <marko.kosunen@aalto.fi>
2017-10-05 17:12:42 -0700

    Add signal generator to f2_system
    Signal Entities/f2_signal_generator created and added to
    Entities/f2_system.
    
    Fixed analyze simple plot in f2_system to automatically scale according
    to maximum outputs of serdes.
    
    Added Entities/modem to provide modulation functions for signal generator.
    
    Added definitions of bbsigdict and ofdmDict to f2_system
    
    Verified with Simulations/F2_System

--- py/f2_signal_gen.py
+++ py/f2_signal_gen.py
@@ -1,11 +1,14 @@
 # f2_signal_gen class 
-# Last modification by Marko Kosunen, marko.kosunen@aalto.fi, 04.10.2017 09:58
+# Last modification by Marko Kosunen, marko.kosunen@aalto.fi, 05.10.2017 16:15
 import numpy as np
 import tempfile
 import subprocess
 import shlex
 import time
 
+import modem as mdm #Function definitions
+
+#Classes are distinguished by the class
 from refptr import *
 from thesdk import *
 from rtl import *
@@ -13,14 +16,16 @@ from rtl import *
 #Simple buffer template
 class f2_signal_gen(thesdk):
     def __init__(self,*arg): 
-        self.proplist = [ 'M' 'K' 'Rs' 'bbsigdict' ];  #Properties that can be propagated from parent
+        self.proplist = [ 'M', 'K', 'Rs', 'bbsigdict', 'ofdmDict' ];  #Properties that can be propagated from parent
         self.Rs = 100e6                                #Sampling frequency
         self.M=4                                       #Number of antennas
         self.K=2                                       #Number of users
-        self.bbsigdict={ 'mode':'sinusoid', 'freqs':[11.0e6 , 13e6, 17e6 ], 'length':2**14 };                     #Mode of the baseband signal. Let's start with sinusoids
+        self.bbsigdict={ 'mode':'sinusoid', 'freqs':[11.0e6 , 13e6, 17e6 ], 'length':2**14 }; #Mode of the baseband signal. Let's start with sinusoids
+        self.ofdmDict={ 'framelen':64,'data_loc': np.r_[1:11+1, 13:25+1, 
+        27:39+1, 41:53+1, 55:64+1]-1, 'pilot_loc' : np.r_[-21, -7, 7, 21] + 32, 'CPlen':16}
         self.model='py';                               #can be set externally, but is not propagated
         self._Z = refptr();
-        self._classfile=__file__
+        self._classfile=__file__ #needed only if rtl defined as superclass
         if len(arg)>=1:
             parent=arg[0]
             self.copy_propval(parent,self.proplist)
@@ -30,14 +35,31 @@ class f2_signal_gen(thesdk):
     def init(self):
         if self.bbsigdict['mode']=='sinusoid':
             self.sinusoid()
+        if self.bbsigdict['mode']=='ofdm_sinusoid':
+            self.ofdm_sinusoid()
 
     def run(self): #Just an alias for init to be consistent: run() always executes the core function
         self.init()
 
+
+    #Methods for signal generation. Add a function and add it to init()
+    #controlled with bbsigdict
     def sinusoid(self):
             length=self.bbsigdict['length']
             phi=np.transpose(np.array(np.mat(self.bbsigdict['freqs'])))*np.array(range(length))*2*2*np.pi/(self.Rs)
             out=np.sum(np.exp(1j*phi),0)/len(self.bbsigdict['freqs'])
             self._Z.Value=np.ones((self.M,1))*out #All antennas emit the same signal
+
+    def ofdm_sinusoid(self):
+            ofdmDict=self.ofdmDict
+            length=np.floor(self.bbsigdict['length']/ofdmDict['framelen'])
+            signalindexes=np.round(np.array(self.bbsigdict['freqs'])/self.Rs*ofdmDict['framelen']).astype(int)
+            frame=np.zeros((length,ofdmDict['framelen']))
+            frame[:,signalindexes]=1
+            datasymbols=frame[:,ofdmDict['data_loc']]
+            pilotsymbols=frame[:,ofdmDict['pilot_loc']]
+            out=np.ones((self.M,1))*mdm.ofdmMod(ofdmDict,datasymbols,pilotsymbols)
+            self._Z.Value=np.ones((self.M,1))*out #All antennas emit the same signal
+
 
 
